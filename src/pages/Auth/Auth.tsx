f27d48a5d07137436b2946cd43b786e45dc6912f
Artur <artur2000sis@gmail.com>
2022-09-16 09:59:42 +0300

    fix

--- src/pages/Auth/Auth.tsx
+++ src/pages/Auth/Auth.tsx
@@ -15,8 +15,8 @@ const Auth: React.FC = () => {
   return (
     <div className={styles.wrapper }>
         <div className={styles.signIn }>
-            <button onClick={signUp} className={styles.btn }>sign up</button>
-            <button onClick={signIn} className={styles.btn }>sign in</button>
+            <button onClick={signUp} className={styles.btn }>зарегистрироватья</button>
+            <button onClick={signIn} className={styles.btn }>войти</button>
         </div>
         {modal === 'signUp' && <Register setState={setModal}/> }
         {modal === 'signIn' && <Login setState={setModal}/> }
